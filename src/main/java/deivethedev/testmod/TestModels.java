b12b843c0dd764dc27750cf9d224701f22b38f22
DeiveTheDev <deivethedev@gmail.com>
2025-02-03 19:29:16 -0300

    ;

--- src/main/java/deivethedev/testmod/TestModels.java
+++ src/main/java/deivethedev/testmod/TestModels.java
@@ -21,17 +21,17 @@ import static deivethedev.testmod.TestMod.MOD_ID;
 import static deivethedev.testmod.TestBlocks.*;
 import static deivethedev.testmod.TestItems.*;
 
-// Please register the model entrypoint in fabric.mod.json:
-
-// "initModels": [
-//	"deivethedev.testmod.TestModels"
-// ],
-
 public class TestModels implements ModelEntrypoint {
 
 	// If the dispatcher arg is conflicting with the override, please update your halplibe to 5.1.2
 	// Go to the file gradle.properties > halplibe_version=5.1.2
 	// Then in the right tab if intelliJ > Gradle > Sync All Gradle Projects (it updates de halplibe)
+
+	// Please register the model entrypoint in fabric.mod.json:
+
+	// "initModels": [
+	//	"deivethedev.testmod.TestModels"
+	// ],
 
 	@Override
 	public void initBlockModels(BlockModelDispatcher dispatcher) {
@@ -53,14 +53,14 @@ public class TestModels implements ModelEntrypoint {
 			model.icon = TextureRegistry.getTexture(new NamespaceID(MOD_ID, "item/banana"));
 			return model;
 		});
-	};
+	}
 
 	@Override
-	public void initEntityModels(EntityRenderDispatcher dispatcher) {};
+	public void initEntityModels(EntityRenderDispatcher dispatcher) {}
 
 	@Override
-	public void initTileEntityModels(TileEntityRenderDispatcher dispatcher) {};
+	public void initTileEntityModels(TileEntityRenderDispatcher dispatcher) {}
 
 	@Override
-	public void initBlockColors(BlockColorDispatcher dispatcher) {};
+	public void initBlockColors(BlockColorDispatcher dispatcher) {}
 }
